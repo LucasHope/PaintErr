24e9991bdfacf366f2a9aea2392501ed46b1d8b7
Antti Pöyhönen <anttipoyhonen1986@gmail.com>
2017-12-04 16:41:23 +0200

    base methods & entity class

--- src/PaintErr/ImageDAO.java
+++ src/PaintErr/ImageDAO.java
@@ -2,5 +2,38 @@ package PaintErr;
 
 import PaintErr.Image;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ImageDAO {
+
+    public static List<Image> list = new ArrayList<>();
+
+    private static Connection con;
+
+    public static List<Image> getAll() {
+
+        // TODO
+
+        return list;
+    }
+
+    public static Image getById(int id) {
+        Image i = new Image();
+
+        // TODO
+
+        return i;
+    }
+
+    public static boolean saveImg(Image i) {
+
+        // TODO
+
+        return true;
+    }
+
+    
+
 }
